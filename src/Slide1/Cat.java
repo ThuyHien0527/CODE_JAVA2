204b1ac40412fe36e631a968d932765c5f7385b6
ThuyHien0527 <thuyhien200755@gmail.com>
2026-03-12 18:33:15 +0700

    Add files via upload

---
+++ src/Slide1/Cat.java
@@ -0,0 +1,11 @@
+package Slide1;
+
+public class Cat extends Animal {
+	public Cat(String name, int age) {
+		super(name,age);
+	}
+	
+	public void keu() {
+		System.out.println(name + "Meow! Meow!");
+	}
+}
---
+++ src/Slide1/Slide1_MayTinh.java
@@ -0,0 +1,10 @@
+package Slide1;
+
+public class Slide1_MayTinh {
+public static void main (String[]args) {
+	MayTinh maytinh=new MayTinh();
+	System.out.println(maytinh.tinhTong(5,5));
+	System.out.println(maytinh.tinhTong(2.5,5));
+	System.out.println(maytinh.tinhTong(4,6,2));
+}
+}
